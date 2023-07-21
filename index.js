031994b110166b89465cec08d9078aeccd544221
박혜민 <hyeminpak@gmail.com>
2023-07-21 18:21:53 +0900

    조건문 No. 9498

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split(" ");
-let a = Number(input[0]);
-let b = Number(input[1]);
-if (a > b) {
-  console.log(">");
-} else if (a < b) {
-  console.log("<");
-} else if (a == b) {
-  console.log("==");
+let input = fs.readFileSync("input.txt").toString();
+input = Number(input);
+if (input >= 90 && 101 > input) {
+  console.log("A");
+} else if (input >= 80 && 90 > input) {
+  console.log("B");
+} else if (input >= 70 && 80 > input) {
+  console.log("C");
+} else if (input >= 60 && 70 > input) {
+  console.log("D");
+} else {
+  console.log("F");
 }
