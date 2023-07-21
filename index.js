59cd02e302aa0d306c3b00e53f1d5b166757ea5e
박혜민 <hyeminpak@gmail.com>
2023-07-21 17:41:54 +0900

    입출력과 사칙연산 No. 10869

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let fs = require("fs");
 let [a, b] = fs.readFileSync("input.txt").toString().split(" ").map(Number);
+console.log(a + b);
+console.log(a - b);
 console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
