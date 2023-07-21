835c26def7c5e1e41cd7b44362d401ee835ca1b2
박혜민 <hyeminpak@gmail.com>
2023-07-21 18:28:33 +0900

    조건문 No. 2884

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString();
-input = Number(input);
-if (input >= 90 && 101 > input) {
-  console.log("A");
-} else if (input >= 80 && 90 > input) {
-  console.log("B");
-} else if (input >= 70 && 80 > input) {
-  console.log("C");
-} else if (input >= 60 && 70 > input) {
-  console.log("D");
+let input = fs.readFileSync("input.txt").toString().split(" ").map(Number);
+let [h, m] = input;
+if (m < 45) {
+  m += 15;
+  h -= 1;
+  if (h < 0) {
+    h = 23;
+  }
 } else {
-  console.log("F");
+  m -= 45;
 }
+console.log(h, m);
