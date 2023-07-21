59e897759423a016b91484c3556b402d53709470
박혜민 <hyeminpak@gmail.com>
2023-07-21 22:24:27 +0900

    일차원배열 No.2562

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split("\n");
-let cnt = Number(input[0]);
-let numbers = input[1].split(" ").map(Number);
-let min = numbers.reduce((a, b) => Math.min(a, b));
-let max = numbers.reduce((a, b) => Math.max(a, b));
-console.log(min, max);
+let input = fs.readFileSync("input.txt").toString().split("\n").map(Number);
+console.log(input.reduce((a, b) => Math.max(a, b)));
+console.log(input.indexOf(input.reduce((a, b) => Math.max(a, b))) + 1);
