8bee2b5f85563a3b167440494134d5a009ac7b69
박혜민 <hyeminpak@gmail.com>
2023-07-21 17:59:29 +0900

    조건문 No. 1330

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split("\n");
+let input = fs.readFileSync("input.txt").toString().split(" ");
 let a = Number(input[0]);
-let b = Number(input[1].split("")[0]);
-let c = Number(input[1].split("")[1]);
-let d = Number(input[1].split("")[2]);
-console.log(a * d);
-console.log(a * c);
-console.log(a * b);
-console.log(a * Number(input[1]));
+let b = Number(input[1]);
+if (a > b) {
+  console.log(">");
+} else if (a < b) {
+  console.log("<");
+} else if (a == b) {
+  console.log("==");
+}
