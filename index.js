b7d1a82968eeda4b63b253cc7810e05c878882a7
박혜민 <hyeminpak@gmail.com>
2023-07-23 17:37:27 +0900

    일차원 배열 No. 1546

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split("\n").map(Number);
-console.log(input.reduce((a, b) => Math.max(a, b)));
-console.log(input.indexOf(input.reduce((a, b) => Math.max(a, b))) + 1);
+let input = fs.readFileSync("input.txt").toString().split("\n");
+let cnt = Number(input[0]);
+let numbers = input[1].split(" ").map(Number);
+let total = numbers.reduce((a, b) => a + b, 0);
+let max = Math.max(...numbers);
+let result = ((total / max) * 100) / cnt;
+console.log(result.toFixed(2));
