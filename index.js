8e984280e3ed66f79968c53c9b8b49f412bba79e
박혜민 <hyeminpak@gmail.com>
2023-07-23 17:44:52 +0900

    문자열 No. 11720

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
-let cnt = Number(input[0]);
-let numbers = input[1].split(" ").map(Number);
-let total = numbers.reduce((a, b) => a + b, 0);
-let max = Math.max(...numbers);
-let result = ((total / max) * 100) / cnt;
-console.log(result.toFixed(2));
+let numbers = input[1].split("").map(Number);
+console.log(numbers.reduce((a, b) => a + b, 0));
