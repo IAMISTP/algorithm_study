a3230f32d9fd3db64a4431284a2dc1e583e340c9
박혜민 <hyeminpak@gmail.com>
2023-07-25 01:55:59 +0900

    정렬 No. 2750

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().split(" ").map(Number);
-function compare(a, b) {
-  return a - b;
+let input = fs.readFileSync("input.txt").toString().split("\n");
+let cnt = Number(input[0]);
+let newArr = [];
+for (let i = 1; i <= cnt; i++) {
+  newArr.push(input[i]);
 }
-input.sort(compare);
-for (let n of input) {
-  console.log(n);
+newArr = newArr.sort((a, b) => a - b);
+for (let i = 0; i < newArr.length; i++) {
+  console.log(newArr[i]);
 }
